7163f6d84aa079a10c74af7eaa570a1b4eb4505b
rokoDev <mishaDoter@gmail.com>
2016-09-21 16:03:43 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,6 +7,7 @@ then
     brew outdated boost || brew upgrade boost;
 elif [ $TRAVIS_OS_NAME = linux ]
 then
+  echo "this is linux blablabla"
   CMAKE_URL="http://www.cmake.org/files/v3.3/cmake-3.3.1-Linux-x86_64.tar.gz"
   mkdir cmake && travis_retry wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
   export PATH=${DEPS_DIR}/cmake/bin:${PATH}
