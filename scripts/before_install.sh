cadbcfb7e7d4342f7df3b8ad0b0224cb81b4df56
rokoDev <mishaDoter@gmail.com>
2016-09-22 15:17:39 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -51,23 +51,23 @@ then
 #Download and install Boost
 #elif [[ ${TRAVIS_OS_NAME} == "linux" && ${BOOST_VERSION} != "default" ]]
 #then
-  if [ ! -f "${DEPS_DIR}/boost/${BOOST_VERSION}_cached" ]
+  if [ ! -f "${DEPS_DIR}/boost/${Boost_VERSION}_cached" ]
   then
     # create dirs for source and install
-    mkdir -p ${DEPS_DIR}/boost${BOOST_VERSION}
+    mkdir -p ${DEPS_DIR}/boost${Boost_VERSION}
     mkdir -p ${DEPS_DIR}/boost
     rm -rf ${DEPS_DIR}/boost/*
     # download
-    travis_retry wget --no-check-certificate --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${BOOST_VERSION}
-    pushd ${DEPS_DIR}/boost${BOOST_VERSION}
+    travis_retry wget --no-check-certificate --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${Boost_VERSION}
+    pushd ${DEPS_DIR}/boost${Boost_VERSION}
     # configure and install
     echo "using gcc : 4.8 : g++-4.8 ;" > $HOME/user-config.jam
     ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=test
     ./b2 -d0 install
     popd
-    touch ${DEPS_DIR}/boost/${BOOST_VERSION}_cached
+    touch ${DEPS_DIR}/boost/${Boost_VERSION}_cached
   else
-    echo 'Using cached Boost ${BOOST_VERSION} libraries.'
+    echo 'Using cached Boost ${Boost_VERSION} libraries.'
   fi
     export CMAKE_OPTIONS=${CMAKE_OPTIONS}" -DBOOST_ROOT=${DEPS_DIR}/boost"
     export BOOST_ROOT=${DEPS_DIR}/boost
