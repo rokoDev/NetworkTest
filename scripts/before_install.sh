4afd3d7ca9651ed626ba07401a39611614b99ce5
rokoDev <mishaDoter@gmail.com>
2016-09-23 02:31:00 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -51,32 +51,67 @@ then
 #Download and install Boost
 #elif [[ ${TRAVIS_OS_NAME} == "linux" && ${BOOST_VERSION} != "default" ]]
 #then
-  if [ ! -f "${DEPS_DIR}/boost/${Boost_VERSION}_cached" ]
-  then
-    # create dirs for source and install
-    mkdir -p ${DEPS_DIR}/boost${Boost_VERSION}
-    mkdir -p ${DEPS_DIR}/boost
-    rm -rf ${DEPS_DIR}/boost/*
-    # download
-    #travis_retry wget --no-check-certificate --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${Boost_VERSION}
-    #travis_retry wget --no-check-certificate -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${Boost_VERSION}
-    travis_retry wget --no-check-certificate -O - ${BOOST_URL} | tar --bzip2 -xf ${DEPS_DIR}/boost${Boost_VERSION}
-    pushd ${DEPS_DIR}/boost${Boost_VERSION}
-    # configure and install
-    echo "using gcc : 4.8 : g++-4.8 ;" > $HOME/user-config.jam
-    ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=test
-    #./b2 -d0 install
-    ./b2 link=static install
-    popd
-    touch ${DEPS_DIR}/boost/${Boost_VERSION}_cached
-  else
-    echo 'Using cached Boost ${Boost_VERSION} libraries.'
-  fi
-    export CMAKE_OPTIONS=${CMAKE_OPTIONS}" -DBOOST_ROOT=${DEPS_DIR}/boost"
-    
-    export BOOST_ROOT=${DEPS_DIR}/boost
-    
-    #export BOOST_ROOT="/opt/boost/boost_1_57_0"
-	export BOOST_INCLUDEDIR="${DEPS_DIR}/boost/include"
-	export BOOST_LIBDIR="${DEPS_DIR}/boost/lib"
+  # if [ ! -f "${DEPS_DIR}/boost/${Boost_VERSION}_cached" ]
+#   then
+#     # create dirs for source and install
+#     mkdir -p ${DEPS_DIR}/boost${Boost_VERSION}
+#     mkdir -p ${DEPS_DIR}/boost
+#     rm -rf ${DEPS_DIR}/boost/*
+#     # download
+#     #travis_retry wget --no-check-certificate --quiet -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${Boost_VERSION}
+#     #travis_retry wget --no-check-certificate -O - ${BOOST_URL} | tar --strip-components=1 -xz -C ${DEPS_DIR}/boost${Boost_VERSION}
+#     travis_retry wget --no-check-certificate -O - ${BOOST_URL} | tar --bzip2 -xf ${DEPS_DIR}/boost${Boost_VERSION}
+#     pushd ${DEPS_DIR}/boost${Boost_VERSION}
+#     # configure and install
+#     echo "using gcc : 4.8 : g++-4.8 ;" > $HOME/user-config.jam
+#     ./bootstrap.sh --prefix=${DEPS_DIR}/boost/ --with-libraries=test
+#     #./b2 -d0 install
+#     ./b2 link=static install
+#     popd
+#     touch ${DEPS_DIR}/boost/${Boost_VERSION}_cached
+#   else
+#     echo 'Using cached Boost ${Boost_VERSION} libraries.'
+#   fi
+#     export CMAKE_OPTIONS=${CMAKE_OPTIONS}" -DBOOST_ROOT=${DEPS_DIR}/boost"
+#     
+#     #export BOOST_ROOT=${DEPS_DIR}/boost
+#     export BOOST_ROOT={DEPS_DIR}/boost/bin
+#     
+#     #export BOOST_ROOT="/opt/boost/boost_1_57_0"
+# 	export BOOST_INCLUDEDIR="${DEPS_DIR}/boost/include"
+# 	export BOOST_LIBDIR="${DEPS_DIR}/boost${Boost_VERSION}/lib"
+  #BOOST_VERSION="1.60.0"
+  #echo "BOOST_VERSION:$BOOST_VERSION"
+
+  BOOST_UNDERSCORE_VERSION=${BOOST_VERSION//./_}
+  #echo "BOOST_UNDERSCORE_VERSION:$BOOST_UNDERSCORE_VERSION"
+
+  mkdir deps
+  DEPS_DIR=deps
+  pushd deps
+
+  mkdir boost
+  pushd boost
+
+  BOOST_URL="http://sourceforge.net/projects/boost/files/boost/$BOOST_VERSION/boost_$BOOST_UNDERSCORE_VERSION.tar.bz2"
+  #echo "BOOST_URL:$BOOST_URL"
+
+  wget $BOOST_URL
+
+  tar --bzip2 -xf "boost_$BOOST_UNDERSCORE_VERSION.tar.bz2"
+  echo "boost archive extracted"
+
+  pushd boost_${BOOST_UNDERSCORE_VERSION}
+
+  ./bootstrap.sh --prefix=../ --with-libraries=test
+  ./b2 -d0 link=static threading=multi install
+
+  popd
+  popd
+  popd
+
+  export BOOST_ROOT="${DEPS_DIR}/boost"
+  export BOOST_INCLUDEDIR="${DEPS_DIR}/boost/include"
+  export BOOST_INCLUDEDIR="${DEPS_DIR}/boost/lib"
+
 fi
