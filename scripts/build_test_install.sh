7bee30feffd1d5785171671b16967633e03578e0
rokoDev <mishaDoter@gmail.com>
2016-09-23 03:18:47 +0300

    minor fix

--- scripts/build_test_install.sh
+++ scripts/build_test_install.sh
@@ -5,7 +5,7 @@ rm -rf build installed
 cmake -E make_directory build
 cd build
 
-cmake .. #-DBOOST_ROOT=$boost_installation_prefix ..
+cmake -DBOOST_INCLUDEDIR="${DEPS_DIR}/boost/include/boost" -DBoost_LIBRARY_DIRS="${DEPS_DIR}/boost/lib" .. #-DBOOST_ROOT=$boost_installation_prefix ..
 cmake --build .
 
 cd tests
