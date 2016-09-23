f9790e7c117a052c4c37351b2fb3ce1f5e756b5e
rokoDev <mishaDoter@gmail.com>
2016-09-23 03:11:03 +0300

    minor fix

--- scripts/build_test_install.sh
+++ scripts/build_test_install.sh
@@ -5,7 +5,7 @@ rm -rf build installed
 cmake -E make_directory build
 cd build
 
-cmake -DBOOST_ROOT=$boost_installation_prefix ..
+cmake .. #-DBOOST_ROOT=$boost_installation_prefix ..
 cmake --build .
 
 cd tests
