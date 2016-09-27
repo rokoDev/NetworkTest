f945c96da9f9a4ff1ff3baaf056f975e2e769046
rokoDev <mishaDoter@gmail.com>
2016-09-28 01:57:58 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -103,19 +103,19 @@ then
 
   pushd boost_${BOOST_UNDERSCORE_VERSION}
 
-  ./bootstrap.sh --prefix=../ --with-libraries=test
+  ./bootstrap.sh --with-libraries=test
   #./b2 -d0 link=static threading=multi install
   #./b2 --layout=versioned -d0 link=static threading=multi runtime-link=static toolset=gcc install
   ./b2 --layout=tagged -d0 link=shared threading=multi install
   
-  pushd ../lib
-  ls
-  popd
+  #pushd ../lib
+  #ls
+  #popd
   
-  echo "include dir contains >>"
-  pushd ../include/boost
-  ls
-  popd
+  #echo "include dir contains >>"
+  #pushd ../include/boost
+  #ls
+  #popd
 
   popd
   popd
