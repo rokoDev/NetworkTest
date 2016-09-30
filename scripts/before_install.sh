e3b5a695d7bbbf3459d0774dac25a03a2e397fe2
rokoDev <mishaDoter@gmail.com>
2016-10-01 01:00:27 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -32,7 +32,10 @@ then
 
   BOOST_URL="http://sourceforge.net/projects/boost/files/boost/$BOOST_VERSION/boost_$BOOST_UNDERSCORE_VERSION.tar.bz2"
   echo "BOOST_URL: $BOOST_URL"
-  gcc -v
+  gcc -version
+  echo "this was gcc compiler version"
+  dpkg --list | grep compiler
+  echo "another version"
 
   wget $BOOST_URL
 
