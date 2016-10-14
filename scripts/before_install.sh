8d354270f5aabf6a4c4f7f50470b1e734cd6b8e1
rokoDev <mishaDoter@gmail.com>
2016-10-14 20:51:12 +0300

    minor fix

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -7,6 +7,10 @@ then
 	#brew update
     #brew outdated cmake || brew upgrade cmake
     #brew outdated boost || brew upgrade boost
+    
+    #find /Applications -maxdepth 1 -name "CMake*" -print
+    #sudo rm /usr/bin/ccmake /usr/bin/cmake /usr/bin/cmake-gui /usr/bin/cmakexbuild /usr/bin/cpack /usr/bin/ctest
+    
     mkdir ~/cmake_tmp
     cd ~/cmake_tmp
     CMAKE_URL="https://cmake.org/files/v${cmake_version_id}/cmake-$cmake_version-Darwin-x86_64.tar.gz"
@@ -14,7 +18,10 @@ then
     tar xf cmake-$cmake_version-Darwin-x86_64.tar.gz
     #sudo cp -R "cmake-$cmake_version-Darwin-x86_64/" "/usr/local/bin"
     CMAKE_PATH=$(pwd)/cmake-$cmake_version-Darwin-x86_64
+    echo "CMAKE_PATH=${CMAKE_PATH}"
     sudo ${CMAKE_PATH}/CMake.app/Contents/bin/cmake-gui --install #=/usr/local/bin
+    cd ..
+    rm -rf ~/cmake_tmp
 elif [ $TRAVIS_OS_NAME = linux ]
 then
   #install cmake--------------------------------------------------------------------
