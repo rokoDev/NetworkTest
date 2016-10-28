3d8222db20bc87694ef80888b3810e5c057a42a2
rokoDev <mishaDoter@gmail.com>
2016-10-28 19:27:41 +0300

    minor fix

--- scripts/build_test_install.sh
+++ scripts/build_test_install.sh
@@ -6,9 +6,9 @@ cmake -E make_directory build
 cd build
 
 #export CC=/usr/local/gcc-6.2.0/bin/gcc-6.2.0
-#export CXX=/usr/local/gcc-6.2.0/bin/g++-6.2.0
+export CXX=/usr/local/gcc-6.2.0/bin/g++-6.2.0
 #export LD=/usr/local/gcc-6.2.0/bin/gcc-6.2.0
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=${COMPILER} -DBUILD_DOCS=ON ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DOCUMENTATION=ON .. #-DCMAKE_CXX_COMPILER=${COMPILER} -DBUILD_DOCS=ON ..
 cmake --build .
 #make doc
 
